fdc426bacfe0da4a3d35ff56ade8157ed68e3c47
jens lindner <jens.lindner@camunda.com>
2023-03-23 08:46:23 +0100

    try own codegen with generatorName=java, library=resttemplate and configOptions.dateLibrary=legacy with failing tests
    (more commented out code as I was reverting local files that I have worked on in a more quick-and-dirty manner)

--- src/test/java/io/m_arc/camunda/test/CamundaInstanceTestApp.java
+++ src/test/java/io/m_arc/camunda/test/CamundaInstanceTestApp.java
@@ -1,12 +1,35 @@
 package io.m_arc.camunda.test;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CamundaInstanceTestApp {
     public static void main(String... args) {
         SpringApplication.run(CamundaInstanceTestApp.class, args);
+    }
+
+    // Format expected from API: 2023-03-21T20:57:30.558Z
+    // exception w/ default: java.lang.Exception: {"type":"InvalidRequestException","message":
+    // "Cannot set query parameter 'after' to value '2023-03-21T20:57:30.558Z':
+    // Cannot convert value \"2023-03-21T20:57:30.558Z\" to java type java.util.Date","code":null}
+
+//    @Bean
+//    public ServletContextInitializer initializer() {
+//        return new ServletContextInitializer() {
+//            @Override
+//            public void onStartup(ServletContext servletContext) throws ServletException {
+//                servletContext.addListener(new CustomJacksonDateFormatListener());
+//                servletContext.setInitParameter("org.camunda.bpm.engine.rest.jackson.dateFormat",
+//                        "yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+//            }
+//        };
+//    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- src/test/java/io/m_arc/camunda/test/DeploymentApiTest.java
+++ src/test/java/io/m_arc/camunda/test/DeploymentApiTest.java
@@ -1,16 +1,15 @@
 package io.m_arc.camunda.test;
 
-
-import org.camunda.community.rest.client.api.DeploymentApi;
-import org.camunda.community.rest.client.invoker.ApiClient;
-import org.camunda.community.rest.client.invoker.ApiException;
+import com.camunda.openapi.api.DeploymentApi;
+import com.camunda.openapi.invoker.ApiClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestClientException;
 
 import java.io.File;
 import java.util.Date;
 
-@SpringBootTest(classes = CamundaInstanceTestApp.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(classes = {CamundaInstanceTestApp.class, DeploymentApi.class, ApiClient.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class DeploymentApiTest {
     private static ApiClient client = new ApiClient();
     private static DeploymentApi api = new DeploymentApi(client);
@@ -27,6 +26,8 @@ public class DeploymentApiTest {
                 null,
                 new File(DeploymentApiTest.class.getClassLoader().getResource(bpmnXmlPath).getFile()));
     }
+
+
     @Test
     public void shouldGetDeployments() throws Exception {
         api.getDeployments(null, null, null, null, null, null, null, null, new Date(), null, null, null, null, null);
@@ -37,8 +38,8 @@ public class DeploymentApiTest {
         try {
             api.getDeployments(null, null, null, null, null, null, null, null, new Date(), null, null, null, null,
                     null);
-        } catch (ApiException e) {
-            throw new Exception(e.getResponseBody());
+        } catch (RestClientException e) {
+            throw new Exception(e.getMessage());
         }
     }
 }
